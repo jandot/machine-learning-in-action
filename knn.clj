aed33b3fd4d1b46d609b8f5f7ffc90c55e45c36c
Jan Aerts <jan.aerts@gmail.com>
2012-06-05 20:03:15 +0200

    kNN digit classification

---
+++ knn.clj
@@ -0,0 +1,26 @@
+(ns knn)
+(use '[leiningen.exec :only (deps)])
+(deps '[[org.clojure/math.numeric-tower "0.0.1"]])
+(use '[clojure.math.numeric-tower])
+
+(load-file "machinelearninginaction/Ch02/trainingDigits/training-set.clj")
+(load-file "machinelearninginaction/Ch02/testDigits/test-set.clj")
+
+(defn calculate-distance [v1 v2]
+	"Calculates distance between 2 vectors. The higher the result, the more different the vectors."
+	(/ (count (filter #(= false %) (map = v1 v2))) (count v1)))
+
+(defn majority-vote [v]
+	"Calculates which item appears most often in a vector. CAUTION: in case two items appear equally often, will pick at random"
+	(first (last (sort-by second (partition 2 (interleave  (set v) (map #(count (filter #{%} v)) (set v))))))))
+
+(defn classify [sample training-set k]
+	(let [distances (pmap #(calculate-distance (:pattern %) sample) training-set)
+		  labels (map :label training-set)
+		  distances-with-labels (map first (take k (sort-by second (partition 2 (interleave labels distances)))))]
+		(majority-vote distances-with-labels)))
+
+(def sample-numbers (map int (take 100 (repeatedly #(rand 946)))))
+(def sample-test-set (map #(nth test-set %) sample-numbers))
+(def predictions (pmap #(classify (:pattern %) training-set 3) sample-test-set))
+(partition 2 (interleave (map :label sample-test-set) predictions))
