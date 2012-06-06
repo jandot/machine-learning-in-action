713221973647cace838ec0dfe56dd7762f2ac792
Jan Aerts <jan.aerts@gmail.com>
2012-06-06 11:47:37 +0200

    Fixed bugs in trees.clj; still until Listing 3.3

--- trees.clj
+++ trees.clj
@@ -4,24 +4,20 @@
 (use '[clojure.math.numeric-tower])
 
 (def data [
-	{:not-surfacing 1 :flippers 1 :fish "yes"}
-	{:not-surfacing 1 :flippers 1 :fish "yes"}
-	{:not-surfacing 1 :flippers 0 :fish "no"}
-	{:not-surfacing 0 :flippers 1 :fish "no"}
-	{:not-surfacing 0 :flippers 1 :fish "no"}])
-
-(defn count-occurences [v]
-	"Counts occurences in a vector"
-	(partition 2 (interleave  (set v) (map #(count (filter #{%} v)) (set v)))))
+	{:not-surfacing 1 :flippers 1 :class "fish"}
+	{:not-surfacing 1 :flippers 1 :class "fish"}
+	{:not-surfacing 1 :flippers 0 :class "not-fish"}
+	{:not-surfacing 0 :flippers 1 :class "not-fish"}
+	{:not-surfacing 0 :flippers 1 :class "not-fish"}])
 
 (defn log2 [n]
 	(/ (Math/log n) (Math/log 2)))
 	
 (defn entropy [m]
 	"Calculates Shannon entropy in dataset m"
-	(let [c (count m)
-		  occurences (count-occurences m)]
-		(reduce - (cons 0 (map #(* (/ (second %) c) (log2 (/ (second %) c))) occurences)))))
+	(let [c (count (map :class m))
+		  occurrences (frequencies (map :class m))]
+		(reduce - (cons 0 (map #(* (/ (second %) c) (log2 (/ (second %) c))) occurrences)))))
 
 (defn split-dataset [m l v]
 	"Takes map m, label l and value v. Returns a subset of a dataset where the value for a key/label l is smaller than value v."
@@ -34,6 +30,8 @@
 		  all-combinations (partition 2 (flatten (map #(interleave (repeat (first %)) (second %)) features-with-values)))
 		  split-datasets (map #(split-dataset m (first %) (second %)) all-combinations)
 		  info-gain (map #(- base-entropy (entropy %)) split-datasets)
-		  info-gain-extended (partition 2 (interleave all-combinations info-gain))]
-		(first (last (sort-by second info-gain-extended)))))
-		
+		  info-gain-extended (group-by first (partition 2 (interleave (map first all-combinations) info-gain)))
+		  labels (keys info-gain-extended)
+		  sums (map #(reduce + (map second %)) (vals info-gain-extended))
+		  ]
+		(first (last (sort-by val (apply assoc {} (interleave labels sums)))))))
